7f711610eae28fd0afad3cab6af988a1c03e228a
Sara Grandfield <sara.grandfield@gmail.com>
2016-11-16 15:41:03 -0500

    Created App_Code File and empty classes. Created Images folder.

---
+++ SGShoesFinal/App_Code/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SGShoesFinal.App_Code;
+
+namespace SGShoesFinal.App_Code
+{
+    public class Category : DBObject
+    {
+    }
+}
---
+++ SGShoesFinal/App_Code/DBObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SGShoesFinal.App_Code
+{
+    public class DBObject
+    {
+        private int _id = 0;
+    }
+
+    public int Id
+    {
+        get { return _id; }
+        set { _id = value; }
+    }
+}
