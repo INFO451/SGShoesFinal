cc9ded44c32310bd54a844c4ad7c0d8050c86c30
Shivang Shah <Shivang Shah@SHIVANGSHAH-HP>
2016-12-04 22:54:35 -0500

    updates

--- SGShoesFinal/LoginPage.aspx.cs
+++ SGShoesFinal/LoginPage.aspx.cs
@@ -7,13 +7,13 @@ using System.Web.UI.WebControls;
 
 namespace SGShoesFinal.Masters
 {
-    public partial class WebForm3 : System.Web.UI.Page
+    public partial class LoginPage : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
             Login1.Focus();
         }
-        protected void Username_Authenticate(object sender, AuthenticateEventArgs e)
+        protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
         {
             if (Login1.UserName == "Admin" && Login1.Password == "Admin")
             {
@@ -27,4 +27,3 @@ namespace SGShoesFinal.Masters
         }
     }
 }
- 
